8102a50279de815b42e4a8331a390f43d138c0a9
kybs00 <1016724958@qq.com>
2025-04-12 23:46:31 +0800

    添加项目文件。

---
+++ WpfApp5/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using Microsoft.Win32;
+
+namespace SessionEndDemo
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            SystemEvents.SessionEnding += SystemEvents_SessionEnding;
+            Application.Current.Exit += Current_Exit;
+            // 在应用程序启动时调用
+            // 0x4FF表示最高优先级，确保你的程序最后被关闭
+            SetProcessShutdownParameters(0x4FF, 0);
+        }
+
+        private void Current_Exit(object sender, ExitEventArgs e)
+        {
+            SystemEvents.SessionEnding -= SystemEvents_SessionEnding;
+        }
+
+        private async void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            if (e.Reason == SessionEndReasons.SystemShutdown)
+            {
+
+                var currentMainWindow = Application.Current.MainWindow;
+                var handle = new WindowInteropHelper(currentMainWindow).Handle;
+                ShutdownBlockReasonCreate(handle, "应用保存数据中，请等待...");
+                //Thread.Sleep(TimeSpan.FromSeconds(10));
+                await Task.Delay(10000);
+                ShutdownBlockReasonCreate(handle, "应用已保存数据");
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                ShutdownBlockReasonDestroy(handle);
+                e.Cancel = false;
+            }
+        }
+        [DllImport("kernel32.dll")]
+        static extern bool SetProcessShutdownParameters(uint dwLevel, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShutdownBlockReasonCreate(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)] string reason);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShutdownBlockReasonDestroy(IntPtr hWnd);
+    }
+}
---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace SessionEndDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= MainWindow_Loaded;
+            Visibility=Visibility.Collapsed;
+        }
+    }
+}

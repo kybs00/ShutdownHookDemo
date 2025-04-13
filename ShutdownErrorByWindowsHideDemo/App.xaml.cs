0fa9006e6faa034bc4175dedd5c319432d341bfd
kybs00 <1016724958@qq.com>
2025-04-14 01:11:04 +0800

    上传关机失败demo

---
+++ ShutdownErrorByWindowsHideDemo/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using Microsoft.Win32;
+
+namespace SessionEndDemo
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            SystemEvents.SessionEnding += SystemEvents_SessionEnding;
+            Application.Current.Exit += Current_Exit;
+        }
+
+        private void Current_Exit(object sender, ExitEventArgs e)
+        {
+            SystemEvents.SessionEnding -= SystemEvents_SessionEnding;
+        }
+
+        private void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            if (e.Reason == SessionEndReasons.SystemShutdown)
+            {
+
+                var currentMainWindow = Application.Current.MainWindow;
+                var handle = new WindowInteropHelper(currentMainWindow).Handle;
+                ShutdownBlockReasonCreate(handle, "应用保存数据中，请等待...");
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                ShutdownBlockReasonCreate(handle, "应用已保存数据");
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                ShutdownBlockReasonDestroy(handle);
+                e.Cancel = false;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool ShutdownBlockReasonCreate(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)] string reason);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShutdownBlockReasonDestroy(IntPtr hWnd);
+    }
+}
---
+++ ShutdownErrorByWindowsHideDemo/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace SessionEndDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //在构造中设置Hide或者Show之后立即设置Hide，均会导致关机阻止失败
+            Hide();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= MainWindow_Loaded;
+            //Loaded之后Hide没问题
+            //Hide();
+            //设置Visibility也没问题
+            //Visibility=Visibility.Collapsed;
+        }
+    }
+}
